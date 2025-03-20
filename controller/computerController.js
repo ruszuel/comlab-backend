27c260c77b66e27adbea2556fc0a06938177b12d
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-03-20 15:46:25 +0800

    add computer

--- controller/computerController.js
+++ controller/computerController.js
@@ -17,4 +17,26 @@ const getList = async(req, res) => {
   }
 }
 
-export default {getList}
+const addComputer = async (req, res) => {
+    const {name, room} = req.body
+    try {
+        const isExisting = await computer.findOne({name})
+        if(isExisting){
+            return res.sendStatus(403)
+        }
+        const newComputer = new computer({name, room})
+        await newComputer.save();
+        res.status(200).json({
+            isSuccess: true,
+            newComputer
+        });
+    } catch (error) {
+        console.error(err);
+        res.status(500).json({
+          isSuccess: false,
+          message: "An error occurred while fetching the data.",
+          error: err.message
+      });
+    }
+}
+export default {getList, addComputer}
--- routes/computerRoutes.js
+++ routes/computerRoutes.js
@@ -4,5 +4,6 @@ import computerController from '../controller/computerController.js';
 const route = express.Router();
 
 route.get('/getList', computerController.getList)
+route.get('/add', computerController.addComputer)
 
 export default route;
