bce2a07e2d45451830fddce3407a375c97c25b41
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-03-20 15:31:35 +0800

    fix model.js

--- controller/computerController.js
+++ controller/computerController.js
@@ -1,15 +1,20 @@
-import { computer } from "../model/model";
+import { computer } from "../model/model.js";
 
 const getList = async(req, res) => {
-    try{
-        const com = await computer.find();
-        res.status(200).json({
-          isSuccess: true,
-          com
-        })
-    }catch(err){
-        console.log(err);
-    }
+  try {
+    const com = await computer.find();
+    res.status(200).json({
+        isSuccess: true,
+        com
+    });
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({
+          isSuccess: false,
+          message: "An error occurred while fetching the data.",
+          error: err.message
+      });
+  }
 }
 
 export default {getList}
