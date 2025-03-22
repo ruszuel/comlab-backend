41779490337fcd012c93e37e9c505ac24173c7f2
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-03-22 13:47:02 +0800

    fixed edit

--- routes/computerStatRoutes.js
+++ routes/computerStatRoutes.js
@@ -6,6 +6,6 @@ const route = express.Router();
 route.get('/getList', computerStatController.getList)
 route.post('/addComputerSet', computerStatController.addComputerSet)
 route.delete('/deleteComputerSet/:_id', computerStatController.deleteComputerSet)
-route.post('/editComputerSet/:_id', computerStatController.deleteComputerSet)
+route.post('/editComputerSet/:_id', computerStatController.editComputerSet)
 
 export default route;
