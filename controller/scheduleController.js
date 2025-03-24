d0f73c8f8a45571ea61850e485b4b1c3b69ca1d4
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-03-24 12:57:12 +0800

    delete sched

--- controller/scheduleController.js
+++ controller/scheduleController.js
@@ -85,21 +85,16 @@ const editSched = async (req, res) => {
 }
 
 const deleteSched = async (req, res) => {
-    const {event_id} = req.body
-    try {
-        const del = await schedule.findOne({event_id})
-        if(!del){
-            return res.sendStatus(404)
+    const {event_id} = req.params
+    try{
+        const delSched = await schedule.deleteOne({event_id});
+        if(delSched.deletedCount === 0){
+            return res.status(404).json({ message: "Schedule not found" });
         }
 
-        const deleting = await schedule.deleteOne({event_id})
-        if(deleting.deletedCount === 0){
-            return
-        }else{
-            return sendStatus(200)
-        }
-    } catch (error) {
-        return res.sendStatus(500)
+        res.status(200).json({ message: "Schedule deleted successfully" });
+    }catch(err){
+        res.status(500).json({ error: error.message });
     }
 }
 
--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -4,7 +4,7 @@ import scheduleController from '../controller/scheduleController.js';
 const route = express.Router()
 
 route.post('/addSchedule', scheduleController.addSchedules)
-route.delete('/deleteSched', scheduleController.deleteSched)
+route.delete('/deleteSched/:event_id', scheduleController.deleteSched)
 route.get('/getSched', scheduleController.getAllSchedules)
 route.put('/updateSched', scheduleController.editSched)
 
